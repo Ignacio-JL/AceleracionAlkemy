da978d66ffa5c2616290614685c04d3c5c114145
Ignacio <joseignacio439@gmail.com>
2022-02-20 19:50:20 -0300

    U1

--- src/main/java/com/alkemy/Aceleracion/entity/ContinentEntity.java
+++ src/main/java/com/alkemy/Aceleracion/entity/ContinentEntity.java
@@ -1,5 +1,24 @@
 package com.alkemy.Aceleracion.entity;
 
-public class ContinentEntity {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "continent")
+@Getter
+@Setter
+public class ContinentEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+
+	private String imagen;
+
+	private String denominacion;
 }
--- src/main/java/com/alkemy/Aceleracion/entity/CountryEntity.java
+++ src/main/java/com/alkemy/Aceleracion/entity/CountryEntity.java
@@ -1,5 +1,68 @@
 package com.alkemy.Aceleracion.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "country")
+@Getter
+@Setter
 public class CountryEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+
+	private String imagen;
+
+	private String denominacion;
+	
+	@Column(name = "cant_habitantes")
+	private Long cantidadHabitantes;
+	
+	private Long superficie;
+	
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "continent_id", insertable = false, updatable = false)
+	private ContinentEntity continent;
+	
+	@Column(name = "continent_id", nullable = false)
+	private Long continentId;
+	
+	@ManyToMany(
+			cascade = {
+					CascadeType.PERSIST,
+					CascadeType.MERGE
+			})
+	@JoinTable(
+			name = "icon_country",
+			joinColumns = @JoinColumn(name = "country_id"),
+			inverseJoinColumns = @JoinColumn(name = "icon_id"))
+	private Set<IconEntity> icons = new HashSet<>();
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null)
+			return false;
+		if(getClass() != obj.getClass())
+			return false;
+		final CountryEntity other = (CountryEntity) obj;
+		return other.id == this.id;
+	}
 
 }
