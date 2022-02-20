db18017b94f6ccc52e22e2ae77fac5efc734d5e9
Ignacio <joseignacio439@gmail.com>
2022-02-20 00:00:22 -0300

    first commit

---
+++ src/test/java/com/alkemy/Aceleracion/AceleracionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.Aceleracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceleracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
